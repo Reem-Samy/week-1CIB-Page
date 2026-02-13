d5a8ac487404321e6828448d23c922e3638af5f8
Reem-Samy <23-101150@students.eui.edu.eg>
2026-02-13 22:13:03 +0200

    first commit

---
+++ cib/src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import "./Nav.css"
+
+
+
+
+const Nav = () => {
+    return ( 
+        <nav className="nav">
+            <ul>
+                <li>homme</li>
+                <li>homme</li>
+                <li>homme</li>
+                <li>homme</li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Nav;
---
+++ cib/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./Home.css"
+import Nav from './../components/Nav';
+
+const  Home= () => {
+    return ( 
+<>
+
+<Nav />
+
+
+</>
+     );
+}
+ 
+export default Home;
